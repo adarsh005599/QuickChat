f2fb882842f7fab7263fb1069c8984ca4e1499b3
adarsh005599 <happysingh005599@gmail.com>
2025-07-24 17:54:27 +0530

    add the frontend to git

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import  LoginPage from './Pages/LoginPage'
+import ProfilePage from './Pages/ProfilePage'
+import {Toaster} from 'react-hot-toast'
+import { AuthContext } from '../context/AuthContext'
+
+
+const App = () => {
+
+  const {authuser} = useContext(AuthContext)
+
+
+  return (
+    <div className="bg-[url('/bgImage.svg')] bg-no-repeat bg-cover">
+
+      <Toaster/>
+
+      <Routes>
+      <Route path='/' element={authuser ? <HomePage/>: <Navigate to="/login"/>}/>
+      <Route path='/login' element={!authuser ? <LoginPage/>: <Navigate to="/"/>}/>
+      <Route path='/profile' element={authuser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
