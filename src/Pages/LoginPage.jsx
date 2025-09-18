4e580ee81cb66aee8ee43aa86129b79ff4a9c892
Adarsh Singh <happysingh005599@gmail.com>
2025-09-19 01:04:59 +0530

    Update LoginPage.jsx

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -29,7 +29,7 @@ const LoginPage = () => {
         initial={{ opacity: 0, y: -40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        src={"/bird.png"}
+        src={"/login-img1.png"}
         className="w-40 sm:w-48 md:w-56"
       />
 
